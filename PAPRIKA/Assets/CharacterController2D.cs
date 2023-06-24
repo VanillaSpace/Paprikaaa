af2f3eee46c2607679d9e33fbd5cde8cbd9246a1
Arisa <aohashi@live.com>
2023-06-24 16:25:15 -0400

    Character anim and tilemap set up

--- PAPRIKA/Assets/CharacterController2D.cs
+++ PAPRIKA/Assets/CharacterController2D.cs
@@ -1,18 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController2D : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
+    [Category("Character")]
+    private Rigidbody2D rigidbody2d;
     [SerializeField] float speed = 2f;
-    Vector2 motionVector;
+    private Vector2 motionVector;
+    
+    [Category("Animators")]
+    private Animator characterAnimator;
+    [SerializeField] Animator[] skinAnimator;
     
     void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        characterAnimator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,6 +28,9 @@ public class CharacterController2D : MonoBehaviour
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
             );
+        
+        TypeOfAnimation(characterAnimator);
+        TypeOfAnimation(skinAnimator[0]); //TODO: Change this into a TMap
     }
 
     void FixedUpdate()
@@ -32,4 +42,19 @@ public class CharacterController2D : MonoBehaviour
     {
        rigidbody2d.velocity = motionVector * speed;
     }
+
+    private void TypeOfAnimation(Animator animator)
+    {
+        animator.SetFloat("horizontal", motionVector.x);
+        animator.SetFloat("vertical", motionVector.y);
+        animator.SetFloat("speed", motionVector.sqrMagnitude);
+    }
+}
+
+internal class cat
+{
+}
+
+internal class cow
+{
 }
